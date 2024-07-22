19eedf3e1096580d700d6053241b61ac562f075c
AfkunG <anthony.g.rivera.i@gmail.com>
2024-07-22 01:46:05 -0500

    Inicio del proyecto Web, creacion de la estructura y clases

---
+++ src/main/java/com/proyecto/ecommerce/ProyectoWebApplication.java
@@ -0,0 +1,14 @@
+package com.proyecto.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ProyectoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoWebApplication.class, args);
+	}
+
+}
