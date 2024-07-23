cad7d0258e436aebf437b0fa101cc4e2e8464a29
AfkunG <anthony.g.rivera.i@gmail.com>
2024-07-23 04:05:24 -0500

    Funcionalidad ve detalle compra user

--- src/main/java/com/proyecto/ecommerce/service/IOrdenService.java
+++ src/main/java/com/proyecto/ecommerce/service/IOrdenService.java
@@ -1,6 +1,7 @@
 package com.proyecto.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.proyecto.ecommerce.model.Orden;
 import com.proyecto.ecommerce.model.Usuario;
@@ -8,9 +9,14 @@ import com.proyecto.ecommerce.model.Usuario;
 public interface IOrdenService {
 	
 	List<Orden> findAll();
+	
+	Optional<Orden> findById(Integer id);
+	
 	Orden save (Orden orden);
 
 	String generarNumeroOrden();
 	
 	List<Orden> findByUsuario (Usuario usuario);
+	
+	
 }
