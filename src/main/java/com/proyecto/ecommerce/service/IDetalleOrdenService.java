5c09c41c1d76ae148edea225e47a7c18e6fdf756
AfkunG <anthony.g.rivera.i@gmail.com>
2024-07-22 22:30:02 -0500

    Creamos los repository y las clases de servicio para la orden y el detalle

---
+++ src/main/java/com/proyecto/ecommerce/service/IDetalleOrdenService.java
@@ -0,0 +1,10 @@
+package com.proyecto.ecommerce.service;
+
+import com.proyecto.ecommerce.model.DetalleOrden;
+
+public interface IDetalleOrdenService {
+	
+	DetalleOrden save (DetalleOrden detalleOrden);
+	
+
+}
